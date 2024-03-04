8e132785a43145c022a4ce445997e15c1a43af75
Nelson Makau Mutua <87982488+nelsonmakau@users.noreply.github.com>
2024-03-04 21:54:04 +0100

    Update Outlier.py

--- Outlier.py
+++ Outlier.py
@@ -1,1 +1,8 @@
-Outliers
+from argparse import ArgumentParser
+import pickle
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+import numpy as np
+from sklearn import metrics
+from sklearn.metrics import *
